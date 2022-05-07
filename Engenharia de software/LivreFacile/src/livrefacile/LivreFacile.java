d6db911630ab828c3aacc78e1e798a5b67f4bd97
Rafael Dalmolin <rafaeldalmolin@live.com>
2022-05-06 23:38:39 -0300

    Engenharia de software

---
+++ Engenharia de software/LivreFacile/src/livrefacile/LivreFacile.java
@@ -0,0 +1,14 @@
+package livrefacile;
+import Funçoes.StringUtil;
+import View.*;
+public class LivreFacile {
+    public static void main(String[] args){
+            //new TelaGerente().setVisible(true);
+            System.out.println(StringUtil.Cripto("facildemais"));
+            System.out.println(StringUtil.Decripto("ÈÃÅËÎ"));
+            System.out.println("teste minusculo");
+
+
+    }
+}      
+
